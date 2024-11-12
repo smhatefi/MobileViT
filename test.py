f51a25210e756093cbbe48ae78892ea3de9fe4bf
smhatefi <smhatefi75@gmail.com>
2024-11-12 20:23:51 +0330

    files modified

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ os.environ["KERAS_BACKEND"] = "torch"
 import torch
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageNet
-from torchvision import datasets
 from torch.utils.data import DataLoader
 from utils.load_weights import return_models
 from utils.dataset import downloadImageNet
@@ -22,15 +21,13 @@ keras_model_XS = keras_model_XS.to(device)
 
 # Define transformation and dataset
 transform = transforms.Compose([
-    transforms.Resize(256),
-    #transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+   transforms.Resize((256, 256)),
+   transforms.ToTensor(),
+   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 downloadImageNet()
 val_dataset = ImageNet(root='./res', split='val', transform=transform)
-#val_dataset = datasets.ImageFolder('res/imagenet/val', transform=transform)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=1)
 
 
@@ -42,6 +39,7 @@ def evaluate_top1_accuracy(model, dataloader):
     with torch.no_grad():
         for images, labels in dataloader:
             images, labels = images.to(device), labels.to(device)
+            images = images.permute(0, 2, 3, 1)  # Convert to (batch, height, width, channels)
             outputs = model(images)
             _, predicted = outputs.max(1)
             total += labels.size(0)
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,10 +1,8 @@
 import os
-import subprocess
 
 def downloadImageNet():
-    os.system("wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz --no-check-certificate -P ./res")
-    os.system("wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar --no-check-certificate -P ./res")
-
-    #os.system("tar -xf ILSVRC2012_img_val.tar")
-    #os.system("tar -xf ILSVRC2012_devkit_t12.tar.gz")
-    #subprocess.call(['sh', './res/extract_ILSVRC.sh'])
+    if not(os.path.exists("./res/ILSVRC2012_devkit_t12.tar.gz")):
+        os.system("wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz --no-check-certificate -P ./res")
+    
+    if not(os.path.exists("./res/ILSVRC2012_img_val.tar")):
+        os.system("wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar --no-check-certificate -P ./res")
